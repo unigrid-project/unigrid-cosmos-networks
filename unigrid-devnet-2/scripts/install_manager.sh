5cffcea335a5ab95f0d8f843e06cc5a52edb2906
dekm <gevan73@gmail.com>
2024-03-13 12:15:43 +0100

    new genesis for devnet with uugd as the base denom

---
+++ unigrid-devnet-2/scripts/install_manager.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# wget -4qO- -o- raw.githubusercontent.com/unigrid-project/unigrid-cosmos-networks/master/unigrid-devnet-1/scripts/install_manager.sh | bash
+
+# Define the URL of the script to download
+SCRIPT_URL="https://raw.githubusercontent.com/unigrid-project/unigrid-cosmos-networks/master/unigrid-devnet-1/scripts/manage_paxd"
+
+# Define the destination filename
+DEST_FILENAME="/usr/local/bin/manage_paxd"
+
+# Download the script
+curl -o "$DEST_FILENAME" "$SCRIPT_URL"
+
+# Check if the download was successful
+if [ $? -eq 0 ]; then
+    echo "Download successful."
+    # Make the script executable
+    chmod +x "$DEST_FILENAME"
+    echo "Script is now executable and located at $DEST_FILENAME"
+else
+    echo "Failed to download the script."
+fi
